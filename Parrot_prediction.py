79f1d709c98bfe79a4cd53d70d36f42eb6efe017
Ivan464 <ivan464@abv.bg>
2024-01-17 21:16:29 +0200

    Maybe

--- Parrot_prediction.py
+++ Parrot_prediction.py
@@ -8,24 +8,28 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 df = pd.read_csv("Parrot.csv")
 df['parrot_name'] = df['parrot_name'].replace({'Budgerigar': 1, 'Eclectus': 2, 'Rose-ringed parakeet': 3})
-correlation_matrix = df.corr()
-plt.show()
-df_corr = df.corr()
-plt.figure(figsize=(10, 8))
-sns.heatmap(df_corr, annot=True, cmap='coolwarm', linewidths=0.5)
-plt.show()
+# correlation_matrix = df.corr()
+# plt.show()
+# df_corr = df.corr()
+# plt.figure(figsize=(10, 8))
+# sns.heatmap(df_corr, annot=True, cmap='coolwarm', linewidths=0.5)
+# plt.show()
 multi_lin = LinearRegression()
-X = df[["age","parrot_name"]]
+X = df[["age", "parrot_name"]]
 y = df["number_of_words"]
-X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=0.8,random_state=6)
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8,random_state=6)
 multi_lin.fit(X_train,y_train)
-# score = multi_lin.score(X_train,y_train)
-# print(f'Train score {score}')
-# score_test = multi_lin.score(X_test,y_test)
-# print(f'Test score {score_test}')
+score = multi_lin.score(X_train,y_train)
+print(f'Train score {score}')
+score_test = multi_lin.score(X_test,y_test)
+print(f'Test score {score_test}')
+parrot = [[12, 1]]
+parrot_pred = multi_lin.predict(parrot)
+print(round(parrot_pred[0]))
 poly = PolynomialFeatures(degree=43)
 X_train_poly = poly.fit_transform(X_train)
 X_test_poly = poly.transform(X_test)
+
 # Standardize the data
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train_poly)
