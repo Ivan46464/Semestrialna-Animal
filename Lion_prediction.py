11b763eb7904bd3c6443032baa8f96e0e77175e5
Ivan464 <ivan464@abv.bg>
2024-02-05 22:04:37 +0200

    Started lion_pred

--- Lion_prediction.py
+++ Lion_prediction.py
@@ -13,10 +13,10 @@ df["health"] = df["health"].replace({"Poor" : 1, "Good" : 2, "Great" : 3, "Excel
 X = df[["age", "weight", "mane_color", "health"]]
 y = df["alpha"]
 scaler = StandardScaler()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=6, test_size=0.3)
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-model = LogisticRegression()
+model = LogisticRegression(class_weight="balanced")
 model.fit(X_train, y_train)
 print(model.score(X_train, y_train))
 print(model.score(X_test, y_test))
@@ -46,3 +46,41 @@ test_conf_matrix = pd.DataFrame(
 )
 
 print(test_conf_matrix)
+x_train_str, x_test_str, y_train_str, y_test_str = train_test_split(X, y, random_state=6, test_size=0.3, stratify=y)
+print('Stratified train positivity rate: ')
+str_train_positivity_rate = sum(y_train_str)/y_train_str.shape[0]
+print(str_train_positivity_rate)
+
+print('Stratified test positivity rate: ')
+str_test_positivity_rate = sum(y_test_str)/y_test_str.shape[0]
+print(str_test_positivity_rate)
+
+
+model.fit(x_train_str, y_train_str)
+y_pred_str = model.predict(x_test_str)
+print(model.score(x_train_str, y_train_str))
+print(model.score(x_test_str, y_test_str))
+coefficients = model.coef_
+intercept = model.intercept_
+
+print('coefficeints: ', coefficients)
+print('intercept: ', intercept)
+
+accuracy1 = accuracy_score(y_test_str, y_pred_str)
+precision1 = precision_score(y_test_str, y_pred_str)
+recall1 = recall_score(y_test_str, y_pred_str)
+f11 = f1_score(y_test_str, y_pred_str)
+
+print(f'Test set accuracy:\t{accuracy1}')
+print(f'Test set precision:\t{precision1}')
+print(f'Test set recall:\t{recall1}')
+print(f'Test set f1-score:\t{f11}')
+
+
+test_conf_matrix1 = pd.DataFrame(
+    confusion_matrix(y_test_str, y_pred_str),
+    index=['actual no', 'actual yes'],
+    columns=['predicted no', 'predicted yes']
+)
+
+print(test_conf_matrix1)
